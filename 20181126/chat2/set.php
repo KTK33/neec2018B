2ccd5df6f93bed0873b0327b7642f99ed6c81b76
Makito Katsube <katsubemakito@gmail.com>
2018-11-26 16:01:44 +0900

    Add files via upload

---
+++ 20181126/chat2/set.php
@@ -0,0 +1,24 @@
+<?php
+require('lib.php');
+
+$uname = $_POST["uname"];
+$msg   = $_POST["msg"];
+$time  = time();
+
+//-------------------------------------------------
+//準備
+//-------------------------------------------------
+// 実行したいSQL
+$sql = 'INSERT INTO log(name,message,time) VALUES(?,?,?)';
+
+//-------------------------------------------------
+//SQLを実行
+//-------------------------------------------------
+$dbh = connectDB();                 //接続
+$sth = $dbh->prepare($sql);         //SQL準備
+$sth->execute([$uname,$msg,date("Y-m-d H:i:s",$time)]);  //実行
+
+
+echo json_encode([
+	"status"=>true
+]);
